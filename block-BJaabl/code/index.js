5c4d832f35fbbf87318b630064807a77ed49e140
nmn <nmngarg030798@gmail.com>
2021-12-12 15:58:54 +0530

    aad

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -2,6 +2,11 @@
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
 
+function sayHello(name){
+  alert(`Hello ${name}`)
+}
+sayHello (`john`);
+
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
 fullName.
@@ -10,6 +15,12 @@ Example:
   getFullName("John", "Snow"); // "John Snow"
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
+
+function getFullName (firstname, lastName){
+  return  ` Hello ${ firstname } ${lastName} `;
+}
+getFullName ("naman", "aggarwal");
+getFullName ("kushal", "Dave");
 
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
@@ -20,6 +31,19 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
+function addTwoNumbers(firstNum, secondNum){
+  if(isNaN(firstNum)===false && isNaN(secondNum)===false){
+    alert(`The sum of both numbers is ${firstNum + secondNum}`)
+  }else{
+    alert(`enter a valid input`)
+  }
+}
+
+addTwoNumbers(22, 12);
+addTwoNumbers(45, 56);
+addTwoNumbers(22, `nmn`);
+addTwoNumbers(`nmn`, 12);
+
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -33,6 +57,21 @@ calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
 
+let numA = +prompt(`enter first num`);
+let numB = +prompt(`enter second num`);
+let operation = prompt(`Operation
+can only be one of these add, sub, mul, div.`)
+if(operation === "add"){
+  alert(`The sum of numbers is ${numA + numB}`)
+}else if (operation === "sub"){
+  alert(`The subtraction of numbers is ${numA - numB}`)
+}else if (operation==="mul"){
+  alert(`The multiplication of numbers is ${numA * numB}`)
+}else if (operation==="div"){
+  alert(`The division of numbers is ${numA / numB}`)
+}else{
+  alert(`enter a valid input.`)
+}
 
 /*
 5. Create a function named `isLeapYear` that accepts a number data type and return `true` or `false` based
@@ -42,7 +81,20 @@ isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
 
+let year = +prompt(`mention a year`)
+if(year%4===0){
+  alert(`This is a leap year`)
+}else{
+  alert(
+    `this is not a leap year`
+  )
+}
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+let numFactorial = +prompt(`enter a number for factorial`)
+for(i=numFactorial; i>=1; i--){
+  numFactorial *= i
+}
+alert(`the factorial of the number is ${numFactorial}`)
